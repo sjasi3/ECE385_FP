156a2d45ac7fc465b8aa85c731097c75b3aee1ce
Sean W Jasin <swjasin03@gmail.com>
2024-12-07 02:31:53 -0600

    Valid logic

--- tetris_logic1/tetris_logic1.srcs/sources_1/new/VPPL.sv
+++ tetris_logic1/tetris_logic1.srcs/sources_1/new/VPPL.sv
@@ -28,8 +28,7 @@
 module VPPL(
     input logic [3:0] Xsi[4],
     input logic [5:0] Ysi[4],
-    input logic [2:0] GXB[4],
-    input logic [2:0] GYB[4],
+    input logic [2:0] eBl[4],
     input logic last_movement,
 
     output logic valid,
@@ -37,4 +36,16 @@ module VPPL(
     );
     // NOTE: To check if the block wraps, check Xsi[0] (center) make sure all
     // blocks are within 4 blocks distance
+    always_comb begin
+        valid = 1;
+        place = 0;
+        if(Xsi[0] - Xsi[1] > 4) valid = 0;
+        if(Xsi[0] - Xsi[2] > 4) valid = 0;
+        if(Xsi[0] - Xsi[3] > 4) valid = 0;
+        if(last_movement == falling) begin
+            if(eBl[0] > 0 | eBl[1] > 0 | eBl[2] > 0 | eBl[3] > 0) place = 1;
+        end else begin
+            if(eBl[0] > 0 | eBl[1] > 0 | eBl[2] > 0 | eBl[3] > 0) valid = 0;
+        end
+    end
 endmodule
