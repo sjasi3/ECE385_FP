660da672c45d7d2a6c73bce63656fc07f0553b93
yurong-wang <122989280+yurong-wang@users.noreply.github.com>
2024-12-07 14:31:35 -0600

    Update KPL.sv

--- tetris_logic1/tetris_logic1.srcs/sources_1/new/KPL.sv
+++ tetris_logic1/tetris_logic1.srcs/sources_1/new/KPL.sv
@@ -27,7 +27,7 @@
 // will seemingly teleport 2 spaces instead of 1. However, the clock signal
 // should be fast enough for this to not be possible
 module KPL(
-    input logic [31:0] keycode,
+    input logic [7:0] keycode,//8 bit keycod
     input logic [3:0] X,
     input logic [5:0] Y,
     input logic [1:0] rType,
@@ -41,11 +41,36 @@ module KPL(
         // TODO: Implement keycodes and their actions
         case (keycode)
             // Any other button press should do nothing
+            8'4a: begin // Left arrow key
+                if (X > 0) // Prevent moving out of bounds
+                    nX = X - 1;
+            end
+            8'h4d: begin // Right arrow key
+                if (X < 9) // Prevent moving out of bounds (assuming 10 columns)
+                    nX = X + 1;
+            end
+            8'h4e: begin // Down arrow key
+                if (Y < 39) // Prevent moving out of bounds (assuming 40 rows)
+                    nY = Y + 1;
+            end
+            8'h04: begin  //a
+                nrType=left;
+            end
+            8'h07: begin  //d
+                nrType=right;
+            end
+            8'h1A: begin  //w
+                nrType=up;
+            end
+            8'h16: begin  //s
+                 nrType=down;
+            end
             default: begin
-                nX <= X;
-                nY <= Y;
-                nrType <= rType;
+                nX = X;
+                nY = Y;
+                nrType = rType;
             end
         endcase
     end
+    
 endmodule
