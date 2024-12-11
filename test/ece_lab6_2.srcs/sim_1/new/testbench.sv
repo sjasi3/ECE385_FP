2de11b185b8786463ea50dd83ec7ca9f96236b78
yurong-wang <122989280+yurong-wang@users.noreply.github.com>
2024-12-11 00:03:53 -0600

    Add files via upload

---
+++ test/ece_lab6_2.srcs/sim_1/new/testbench.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/13/2024 07:41:31 PM
+// Design Name: 
+// Module Name: testbench
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module testbench(
+
+    );
+endmodule
